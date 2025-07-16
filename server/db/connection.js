6c5915060421a025f4a656966bec7aa8b5da8f8f
Priyanka <lotiyapiya@gmail.com>
2025-07-16 13:10:09 +0530

    "pdf parse"

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: 'localhost',
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
